7b4704e4f5045e8ce68bf78b094150e7cdd5b631
Martin von Gagern <Martin.vGagern@gmx.net>
2016-03-07 11:20:20 +0100

    Switch from Selenium to direct HTTP requests
    This is faster, can be performed headless, and avoids trouble dealing with
    dynamic HTML5 pages and the delays encountered waiting for data.  It might
    further restrict the range of permissible OS versions, though.
    
    This is known to work with a Fritz!Box 7490 running Fritz!OS 06.51.

--- index.js
+++ index.js
@@ -1,24 +1,105 @@
-"use strict";
+'use strict';
 
-var cfg = require("./config.json");
+const request = require('request');
+const md5 = require('md5');
+const fs = require('fs');
 
-var webdriver = require("selenium-webdriver");
-var until = webdriver.until;
-var driver = new webdriver.Builder()
-    .forBrowser("firefox")
-    .build();
+const cfg = require('./config.json');
+const caCert = fs.readFileSync(require.resolve('./ca.pem'), 'utf-8');
 
-driver.get(cfg.url);
-var tdMachine = "td[title='" + cfg.machine + "']";
-driver.switchTo().frame(0);
-driver.findElement({id: "uiViewUser"}).sendKeys(cfg.user);
-driver.findElement({id: "uiPass"}).sendKeys(cfg.pass);
-driver.findElement({id: "uiSubmitLogin"}).click();
-driver.findElement({linkText: "Heimnetz"}).click();
-driver.findElement({css: tdMachine + " ~ td button"}).click();
-driver.findElement({id: "uiBtnWake"}).click();
-driver.findElement({css: tdMachine});
-driver.findElement({id: "sso_dropdown"}).click();
-driver.findElement({linkText: "Abmelden"}).click();
-driver.findElement({id: "uiViewUser"});
-driver.close();
+console.log('Requesting login page');
+request.get({
+    url: cfg.url,
+    agentOptions: {
+        ca: caCert,
+    },
+}, login);
+
+function post(page, data, next) {
+    request.post({
+        url: cfg.url.replace(/\/$/, '') + '/' + page,
+        form: data,
+        agentOptions: {
+            ca: caCert,
+        },
+    }, next);
+}
+
+function login(err, resp, body) {
+    if (err) throw err;
+    let m = /"challenge": *"([^"]*)"/.exec(body);
+    let challenge = m[1];
+    let c = challenge + '-';
+    let pass = cfg.pass.replace(/[^\u0000-\u00ff]/g, '.');
+    let bytes = (c + pass).replace(/[^]/g, '$&\0');
+    let response = c + md5(bytes);
+    let data = {
+        response: response,
+        username: cfg.user,
+        lp: '',
+    };
+    console.log('Sending login credentials');
+    post('/', data, netDev);
+}
+
+function netDev(err, resp, body) {
+    if (err) throw err;
+    let m = /"sid": *"([^"]*)"/.exec(body);
+    let sid = m[1];
+    let data = {
+        xhr: 1,
+        sid: sid,
+        lang: 'de',
+        page: 'netDev',
+        type: 'all',
+        no_siderenew: '',
+    };
+    console.log('Listing network devices');
+    post('data.lua', data, boot);
+}
+
+function boot(err, resp, body) {
+    if (err) throw err;
+    var json = JSON.parse(body);
+    var m = json.data.active.concat(json.data.passive)
+        .filter(x => x.name === cfg.machine);
+    if (m.length === 0) throw Error("Machine not found");
+    if (m.length > 1) throw Error("Multiple matches found");
+    m = m[0];
+    let data = {
+        xhr: 1,
+        sid: json.sid,
+        lang: 'de',
+        no_sidrenew: '',
+        plc_desc: m.name,
+        dev_name: m.name,
+        btn_reset_name: '',
+        static_dhcp: 'on',
+        kisi_profile: 'filtprof1',
+        back_to_page: '/net/net_overview.lua',
+        dev: m.UID,
+        last_action: '',
+        btn_wake: '',
+        oldpage: '/net/edit_device.lua',
+    };
+    console.log('Booting device');
+    post('data.lua', data, logout);
+}
+
+function logout(err, resp, body) {
+    if (err) throw err;
+    var json = JSON.parse(body);
+    let data = {
+        xhr: 1,
+        sid: json.sid,
+        logout: 1,
+        no_sidrenew: '',
+    };
+    console.log('Logging out');
+    post('index.lua', data, done);
+}
+
+function done(err, resp, body) {
+    if (err) throw err;
+    console.log('Done');
+}
